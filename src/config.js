b3bb7199718176054b33016cea06ed941a0eaeb1
rob <rca06d@my.fsu.edu>
2019-04-14 10:45:29 -0600

    inject environment vars

---
+++ src/config.js
@@ -0,0 +1,3 @@
+/* eslint no-undef: 0 */
+// Values found here such as API_URL are added by webpack (see DefinePlugin entries)
+export const ApiURL = API_URL;
--- webpack.dev.js
+++ webpack.dev.js
@@ -1,4 +1,15 @@
 const path = require('path');
+const { DefinePlugin } = require('webpack');
+require('dotenv').config();
+
+const getEnvVar = (name, { optional } = { optional: false }) => {
+	const envVar = process.env[name];
+	if (!optional && !envVar) {
+		throw new Error(`Missing env var ${name} . Did you forget to add it to a .env file?`);
+	}
+
+	return JSON.stringify(envVar);
+};
 
 module.exports = {
 	entry: './src/index.js',
@@ -17,9 +28,18 @@ module.exports = {
 			},
 		],
 	},
+	plugins: [
+		// webpack essentially does a find and replace on each key listed here. API_URL cannot be accessed
+		// in the app via global.API_URL, process.env.API_URL, or any way other than plain 'ol API_URL.
+		// If you define something here like 'process.env.API_URL', it will only work if you access it in app
+		// by explicitly writing out 'process.env.API_URL'. It won't work if you do const { API_URL } = process.env;
+		new DefinePlugin({
+			API_URL: getEnvVar('API_URL'),
+		}),
+	],
 	devServer: {
 		contentBase: path.join(__dirname, 'public/'),
-		port: 3000,
+		port: process.env.PORT,
 		publicPath: 'http://localhost:3000/dist/',
 		hotOnly: true,
 	},
