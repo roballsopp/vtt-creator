ae2f1e227d57a987ff7b8f229537cf62c0a61c2f
rob <rca06d@my.fsu.edu>
2019-05-24 16:31:37 -0600

    basic video loader/player

---
+++ src/video.component.js
@@ -0,0 +1,49 @@
+import * as React from 'react';
+import * as PropTypes from 'prop-types';
+import { makeStyles } from '@material-ui/styles';
+import FileSelector from './file-selector.component';
+
+const useStyles = makeStyles({
+	container: {
+		display: 'flex',
+		backgroundColor: 'black',
+		justifyContent: 'center',
+		alignItems: 'center',
+	},
+});
+
+Video.propTypes = {
+	width: PropTypes.number,
+	height: PropTypes.number,
+	onFileSelected: PropTypes.func.isRequired,
+};
+
+Video.defaultProps = {
+	width: 600,
+	height: 450,
+};
+
+export default function Video(props) {
+	const { width, height } = props;
+	const [src, setSrc] = React.useState();
+	const [captionSrc, setCaptionSrc] = React.useState();
+	const classes = useStyles();
+
+	const onFileSelected = file => {
+		const localUrl = URL.createObjectURL(file);
+		setSrc(localUrl);
+		props.onFileSelected(file);
+	};
+
+	return (
+		<div className={classes.container} style={{ width, height }}>
+			{!src && <FileSelector label="Select Video File" accept="video/*" onFileSelected={onFileSelected} />}
+			{src && (
+				<video width={width} height={height} controls>
+					<source src={src} />
+					<track src={captionSrc} kind="subtitles" srcLang="en" label="English" />
+				</video>
+			)}
+		</div>
+	);
+}
