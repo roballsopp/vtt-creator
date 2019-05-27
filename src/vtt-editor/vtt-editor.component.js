207869533d8616447ebb15ecd12edef405ee3730
rob <rca06d@my.fsu.edu>
2019-05-27 15:53:52 -0600

    captions are downloadable as VTT

--- src/vtt-editor/vtt-editor.component.js
+++ src/vtt-editor/vtt-editor.component.js
@@ -1,10 +1,21 @@
 import * as React from 'react';
 import * as PropTypes from 'prop-types';
+import AppBar from '@material-ui/core/AppBar';
 import Button from '@material-ui/core/Button';
 import Grid from '@material-ui/core/Grid';
+import IconButton from '@material-ui/core/IconButton';
+import Menu from '@material-ui/core/Menu';
+import MenuItem from '@material-ui/core/MenuItem';
 import Paper from '@material-ui/core/Paper';
+import Toolbar from '@material-ui/core/Toolbar';
+import Typography from '@material-ui/core/Typography';
+import MoreIcon from '@material-ui/icons/MoreVert';
+import CloudDownloadIcon from '@material-ui/icons/CloudDownload';
+import CloudUploadIcon from '@material-ui/icons/CloudUpload';
+import VoiceChatIcon from '@material-ui/icons/VoiceChat';
 import { makeStyles } from '@material-ui/styles';
-import { CuePropType } from '../services/vtt.service';
+import download from 'downloadjs';
+import { CuePropType, getVTTFromCues } from '../services/vtt.service';
 import CueEditor from './cue-editor.component';
 
 const useStyles = makeStyles({
@@ -12,6 +23,9 @@ const useStyles = makeStyles({
 		width: 400,
 		height: '100%',
 		padding: 16,
+	},
+	menuIcon: {
+		marginRight: 16,
 	},
 });
 
@@ -22,6 +36,7 @@ VTTEditor.propTypes = {
 
 export default function VTTEditor({ cues, onChange }) {
 	const classes = useStyles();
+	const [optionsMenuAnchorEl, setOptionsMenuAnchorEl] = React.useState(null);
 
 	const onChangeCue = i => cue => {
 		const newCues = cues.slice();
@@ -43,22 +58,61 @@ export default function VTTEditor({ cues, onChange }) {
 		return onChange(newCues);
 	};
 
+	const onCloseOptionsMenu = () => {
+		setOptionsMenuAnchorEl(null);
+	};
+
+	const onDownloadVTT = () => {
+		download(getVTTFromCues(cues), 'my_captions.vtt', 'text/vtt');
+		onCloseOptionsMenu();
+	};
+
 	return (
-		<Paper className={classes.container}>
-			<Grid container spacing={8}>
-				{cues.map((cue, i) => {
-					return (
-						<Grid key={i} item xs={12}>
-							<CueEditor cue={cue} cueNumber={i + 1} onChange={onChangeCue(i)} onDelete={onRemoveCue(i)} />
-						</Grid>
-					);
-				})}
-				<Grid item xs={12}>
-					<Button variant="contained" color="primary" fullWidth onClick={onAddCue}>
-						Add Cue
-					</Button>
+		<Paper>
+			<AppBar position="static" color="primary">
+				<Toolbar>
+					<Typography variant="h6" color="inherit" style={{ flexGrow: 1 }}>
+						Caption List
+					</Typography>
+					<IconButton
+						edge="end"
+						color="inherit"
+						aria-label="Menu"
+						onClick={e => setOptionsMenuAnchorEl(e.currentTarget)}>
+						<MoreIcon />
+					</IconButton>
+					<Menu anchorEl={optionsMenuAnchorEl} open={!!optionsMenuAnchorEl} onClose={onCloseOptionsMenu}>
+						<MenuItem>
+							<CloudUploadIcon className={classes.menuIcon} />
+							Load from VTT file...
+						</MenuItem>
+						<MenuItem>
+							<VoiceChatIcon className={classes.menuIcon} />
+							Extract from video...
+						</MenuItem>
+						<MenuItem onClick={onDownloadVTT}>
+							<CloudDownloadIcon className={classes.menuIcon} />
+							Save to VTT file...
+						</MenuItem>
+					</Menu>
+				</Toolbar>
+			</AppBar>
+			<div className={classes.container}>
+				<Grid container spacing={8}>
+					{cues.map((cue, i) => {
+						return (
+							<Grid key={i} item xs={12}>
+								<CueEditor cue={cue} cueNumber={i + 1} onChange={onChangeCue(i)} onDelete={onRemoveCue(i)} />
+							</Grid>
+						);
+					})}
+					<Grid item xs={12}>
+						<Button variant="contained" color="primary" fullWidth onClick={onAddCue}>
+							Add Cue
+						</Button>
+					</Grid>
 				</Grid>
-			</Grid>
+			</div>
 		</Paper>
 	);
 }
