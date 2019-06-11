dbbc7f804a687734e354efe56fbc6635fb81eb54
rob <rca06d@my.fsu.edu>
2019-06-10 22:41:37 -0600

    use multiple svgs to avoid max size issue

--- src/vtt-timeline/time-ticks.component.js
+++ src/vtt-timeline/time-ticks.component.js
@@ -1,38 +1,86 @@
 import * as React from 'react';
 import * as PropTypes from 'prop-types';
 import muiGreys from '@material-ui/core/colors/grey';
+import { makeStyles } from '@material-ui/styles';
 import { useZoom } from './zoom-container.component';
 import { formatSeconds } from '../services/vtt.service';
 
+const useStyles = makeStyles({
+	root: {
+		display: 'flex',
+	},
+});
+
 TimeTicks.propTypes = {
+	svgSize: PropTypes.number,
 	height: PropTypes.number,
 };
 
 TimeTicks.defaultProps = {
+	svgSize: 5000,
 	height: 20,
 };
 
-export default function TimeTicks({ height }) {
+export default function TimeTicks({ height, svgSize }) {
+	const classes = useStyles();
 	// zoom is pixels/second
 	const { zoom, zoomContainerRect } = useZoom();
 
-	// one tick per second
-	const numTicks = Math.ceil(zoomContainerRect.width / zoom);
+	// keep svgs to a reasonable size
+	const numSvgs = Math.floor(zoomContainerRect.width / svgSize);
+	const numTicks = Math.ceil(svgSize / zoom);
 
+	const svgs = [];
+	let elapsedPixels = 0;
+
+	for (let i = 0; i < numSvgs; i++) {
+		const tickOffset = -(elapsedPixels % zoom);
+		const startTickValue = numTicks * i;
+		svgs.push(
+			<SvgSegment
+				key={i}
+				width={svgSize}
+				height={height}
+				startTickValue={startTickValue}
+				tickOffset={tickOffset}
+				pixelsPerTick={zoom}
+			/>
+		);
+		elapsedPixels += svgSize;
+	}
+
+	const lastSvgSize = zoomContainerRect.width % svgSize;
+
+	if (lastSvgSize) {
+		const tickOffset = -(elapsedPixels % zoom);
+		const startTickValue = numTicks * numSvgs;
+		svgs.push(
+			<SvgSegment
+				key="final"
+				width={lastSvgSize}
+				height={height}
+				startTickValue={startTickValue}
+				tickOffset={tickOffset}
+				pixelsPerTick={zoom}
+			/>
+		);
+	}
+
+	return <div className={classes.root}>{svgs}</div>;
+}
+
+function SvgSegment({ width, height, startTickValue, tickOffset, pixelsPerTick, ...props }) {
+	// one tick per second, and add one so it overlaps with the starting tick of the next svg
+	const numTicks = Math.ceil(width / pixelsPerTick) + 1;
 	const ticks = [];
 
-	// skip first tick
-	for (let i = 1; i < numTicks; i++) {
-		ticks.push(<Tick key={i} x={i * zoom} height={height} text={formatSeconds(i)} />);
+	for (let i = 0; i < numTicks; i++) {
+		const x = tickOffset + i * pixelsPerTick;
+		ticks.push(<Tick key={i} x={x} height={height} text={formatSeconds(i + startTickValue)} />);
 	}
 
 	return (
-		<svg
-			version="1.1"
-			baseProfile="full"
-			width={zoomContainerRect.width}
-			height={height}
-			xmlns="http://www.w3.org/2000/svg">
+		<svg {...props} width={width} height={height} xmlns="http://www.w3.org/2000/svg">
 			<rect width="100%" height="100%" fill={muiGreys[600]} />
 			{ticks}
 		</svg>
