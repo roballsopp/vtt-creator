04615d26946447b88dc516fbc40ee26f885d13bb
Rob Allsopp <rallsopp@commercialtribe.com>
2019-06-14 17:46:43 -0600

    handle video errors

--- src/common/video/video-dom.context.js
+++ src/common/video/video-dom.context.js
@@ -1,5 +1,6 @@
 import * as React from 'react';
 import * as PropTypes from 'prop-types';
+import { useToast } from '../toast-context';
 
 const VideoDomContext = React.createContext({});
 
@@ -8,8 +9,24 @@ VideoDomProvider.propTypes = {
 };
 
 export function VideoDomProvider({ children }) {
+	const toast = useToast();
 	const [videoRef, onVideoRef] = React.useState();
 	const [videoContainerRef, onVideoContainerRef] = React.useState();
+
+	React.useEffect(() => {
+		if (!videoRef) return;
+
+		const onError = () => {
+			toast.error('Something went wrong with the video.');
+			console.error(videoRef.error);
+		};
+
+		videoRef.addEventListener('error', onError);
+
+		return () => {
+			videoRef.removeEventListener('error', onError);
+		};
+	}, [toast, videoRef]);
 
 	return (
 		<VideoDomContext.Provider
