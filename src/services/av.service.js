85178302b4409b8f1a6f9860b350d3e8599cb3fd
Rob Allsopp <rallsopp@commercialtribe.com>
2019-06-14 18:01:40 -0600

    webkit prefix, and don't use promise-based syntax for safari compatibility

--- src/services/av.service.js
+++ src/services/av.service.js
@@ -1,26 +1,31 @@
 const encodeWAV = require('audiobuffer-to-wav');
 
 const SAMPLE_RATE = 44100;
+const AudioContext = window.AudioContext || window.webkitAudioContext;
+const OfflineAudioContext = window.OfflineAudioContext || window.webkitOfflineAudioContext;
 
 export const getAudioBlobFromVideo = async file => {
 	return new Promise((resolve, reject) => {
 		const reader = new FileReader();
 
-		reader.addEventListener('load', async () => {
-			try {
-				const decodeCtx = new AudioContext({ sampleRate: SAMPLE_RATE });
-				const decodedBuffer = await decodeCtx.decodeAudioData(reader.result);
-				// TODO: all this garbage just to convert to mono...i think...improvements?
-				const offlineCtx = new OfflineAudioContext(1, decodedBuffer.length, SAMPLE_RATE);
-				const soundSource = offlineCtx.createBufferSource();
-				soundSource.buffer = decodedBuffer;
-				soundSource.connect(offlineCtx.destination);
-				soundSource.start();
-				const audioBuffer = await offlineCtx.startRendering();
-				resolve(encodeWAV(audioBuffer));
-			} catch (e) {
-				reject(e);
-			}
+		reader.addEventListener('load', () => {
+			const decodeCtx = new AudioContext({ sampleRate: SAMPLE_RATE });
+			decodeCtx.decodeAudioData(
+				reader.result,
+				decodedBuffer => {
+					// TODO: all this garbage just to convert to mono...i think...improvements?
+					const offlineCtx = new OfflineAudioContext(1, decodedBuffer.length, SAMPLE_RATE);
+					const soundSource = offlineCtx.createBufferSource();
+					soundSource.buffer = decodedBuffer;
+					soundSource.connect(offlineCtx.destination);
+					soundSource.start();
+					offlineCtx.addEventListener('complete', e => {
+						resolve(encodeWAV(e.renderedBuffer));
+					});
+					offlineCtx.startRendering();
+				},
+				resp => reject(resp.err)
+			);
 		});
 
 		reader.readAsArrayBuffer(file);
