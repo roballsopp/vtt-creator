753428aad55f26ec86ee4a903a60524b3c184e40
rob <rca06d@my.fsu.edu>
2019-06-21 16:20:58 -0600

    deployment script tweaks

--- scripts/deploy.sh
+++ scripts/deploy.sh
@@ -1,6 +1,8 @@
 #!/bin/bash -e
 cd /var/www/vtt-creator.com
+git reset --hard
 git fetch --tags "https://$USERNAME:$PASSWORD@github.com/roballsopp/vtt-creator.git"
 git checkout $TAG
+chmod +x  scripts/*
 yarn
 yarn build
