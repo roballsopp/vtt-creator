8f62a125bbf13536257648a180dc1d4e65826a5e
rob <rca06d@my.fsu.edu>
2019-06-26 14:10:08 -0600

    feature overview

--- src/splash/splash.component.js
+++ src/splash/splash.component.js
@@ -2,6 +2,9 @@ import * as React from 'react';
 import * as PropTypes from 'prop-types';
 import Typography from '@material-ui/core/Typography';
 import { makeStyles } from '@material-ui/styles';
+import SubtitlesIcon from '@material-ui/icons/Subtitles';
+import MemoryIcon from '@material-ui/icons/Memory';
+import CloudDownloadIcon from '@material-ui/icons/CloudDownload';
 import { Button } from '../common';
 
 const useStyles = makeStyles({
@@ -9,6 +12,7 @@ const useStyles = makeStyles({
 		display: 'flex',
 		flexDirection: 'column',
 		flex: 1,
+		alignItems: 'center',
 	},
 	banner: {
 		display: 'flex',
@@ -16,12 +20,37 @@ const useStyles = makeStyles({
 		justifyContent: 'center',
 		alignItems: 'center',
 		height: 300,
+		width: '100%',
 		backgroundColor: 'gray',
 	},
-	header: {
+	bannerInfo: {
+		display: 'flex',
+		justifyContent: 'space-between',
+		width: 1024,
 		color: 'white',
-		width: 700,
-		marginRight: 50,
+	},
+	bannerButton: {
+		display: 'flex',
+		justifyContent: 'center',
+		alignItems: 'center',
+	},
+	content: {
+		flex: 1,
+		display: 'flex',
+		justifyContent: 'center',
+		alignItems: 'center',
+	},
+	features: {
+		display: 'flex',
+		justifyContent: 'space-between',
+		width: 1024,
+	},
+	feature: {
+		width: 300,
+		textAlign: 'center',
+	},
+	featureIcon: {
+		fontSize: 80,
 	},
 });
 
@@ -35,16 +64,40 @@ export default function Splash({ history }) {
 	return (
 		<div className={classes.root}>
 			<div className={classes.banner}>
-				<div className={classes.header}>
-					<Typography variant="h4">Caption your videos online for free.</Typography>
-					<Typography variant="h6">
-						VTT Creator is an online editor and visualizer for the WebVTT file format.
-					</Typography>
+				<div className={classes.bannerInfo}>
+					<div>
+						<Typography variant="h4">Caption your videos online for free.</Typography>
+						<Typography variant="h6">
+							VTT Creator is an online editor and visualizer for the WebVTT file format.
+						</Typography>
+					</div>
+					<div className={classes.bannerButton}>
+						<Button size="large" variant="contained" color="secondary" onClick={() => history.push('/editor')}>
+							Create Captions
+						</Button>
+					</div>
 				</div>
-				<div>
-					<Button size="large" variant="contained" color="secondary" onClick={() => history.push('/editor')}>
-						Create Captions
-					</Button>
+			</div>
+			<div className={classes.content}>
+				<div className={classes.features}>
+					<div className={classes.feature}>
+						<SubtitlesIcon className={classes.featureIcon} />
+						<Typography variant="h6">
+							Load a video, and see your captions in action as you edit.
+						</Typography>
+					</div>
+					<div className={classes.feature}>
+						<MemoryIcon className={classes.featureIcon} />
+						<Typography variant="h6">
+							Leverage machine learning to automatically extract captions directly from your video.
+						</Typography>
+					</div>
+					<div className={classes.feature}>
+						<CloudDownloadIcon className={classes.featureIcon} />
+						<Typography variant="h6">
+							Export your work to a .vtt file for use anywhere on the web.
+						</Typography>
+					</div>
 				</div>
 			</div>
 		</div>
