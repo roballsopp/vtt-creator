1665463f42f09a6c3c0f3f143ea84e063690491a
rob <rca06d@my.fsu.edu>
2019-12-03 16:50:37 -0700

    move off of babel polyfill

--- src/polyfills.js
+++ src/polyfills.js
@@ -1,4 +1,8 @@
-import '@babel/polyfill';
+import 'regenerator-runtime/runtime';
+import 'core-js/es/map';
+import 'core-js/es/set';
+import 'core-js/es/promise';
+import 'core-js/es/object';
 import { VTTCue } from 'vtt.js';
 
 window.VTTCue = VTTCue;
