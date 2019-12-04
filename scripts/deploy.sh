398de9264cce5c0eb35e1f130a5f0e27a36fcf9c
rob <rca06d@my.fsu.edu>
2019-12-04 15:14:27 -0700

    dont need the deploy script anymore

--- scripts/deploy.sh
+++
@@ -1,7 +0,0 @@
-#!/bin/bash -e
-cd /var/www/vtt-creator.com
-git reset --hard
-git fetch --tags "https://$USERNAME:$PASSWORD@github.com/roballsopp/vtt-creator.git"
-git checkout $TAG
-yarn
-yarn build
