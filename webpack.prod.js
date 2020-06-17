87aee63af5cff3c54fb204754f57e34051c2f54d
rob <rca06d@my.fsu.edu>
2020-06-17 10:07:05 -0600

    netlify plugin might already do this if I actually output source maps from webpack...

--- webpack.prod.js
+++ webpack.prod.js
@@ -2,13 +2,14 @@ const path = require('path');
 const UglifyJsPlugin = require('uglifyjs-webpack-plugin');
 const { DefinePlugin } = require('webpack');
 const HtmlWebpackPlugin = require('html-webpack-plugin');
-const SentryWebpackPlugin = require('@sentry/webpack-plugin');
 
 module.exports = {
 	entry: {
 		main: ['./src/full-story', './src/google-analytics', './src/polyfills', './src/doorbell', './src/index.js'],
 	},
 	mode: 'production',
+	// this setting and UglifyJsPlugin({ sourceMap: true }) necessary to output source maps
+	devtool: 'source-map',
 	output: {
 		// the filename will just be the corresponding key from the entry object above (entry.main becomes docs/main.js)
 		path: path.resolve(__dirname, 'public'),
@@ -42,13 +43,8 @@ module.exports = {
 			filename: path.join(__dirname, 'public', 'index.html'),
 			chunks: ['main'],
 		}),
-		new SentryWebpackPlugin({
-			include: '.',
-			ignore: ['node_modules', 'docs', 'webpack.prod.js', 'webpack.dev.js', 'webpack.demo.js'],
-			configFile: 'sentry.properties',
-		}),
 	],
 	optimization: {
-		minimizer: [new UglifyJsPlugin()],
+		minimizer: [new UglifyJsPlugin({ sourceMap: true })],
 	},
 };
