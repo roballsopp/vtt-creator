2298cd77cc38a09ba0678bce09bfaa1ea8be60b8
rob <rca06d@my.fsu.edu>
2020-06-25 13:52:25 -0600

    coerce debug to boolean

--- src/config.js
+++ src/config.js
@@ -5,6 +5,6 @@ export const apiDisabled = !API_URL;
 export const donationDisabled = !(PAYPAL_CLIENT_ID && API_URL);
 export const SpeechToTextJobTimeout = SPEECH_TO_TEXT_JOB_TIMEOUT || 20000;
 export const SentryDSN = SENTRY_DSN;
-export const DebugMode = DEBUG;
+export const DebugMode = Boolean(DEBUG);
 export const GAProduct = GA_PRODUCT;
 export const PayPalClientID = PAYPAL_CLIENT_ID;
