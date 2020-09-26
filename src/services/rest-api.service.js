c818c25a4f56e47c9b4cce82b3b1c3036054d948
rob <rca06d@my.fsu.edu>
2020-09-26 10:33:19 -0600

    clearer upload errors

--- src/services/rest-api.service.js
+++ src/services/rest-api.service.js
@@ -4,9 +4,16 @@ export const uploadFile = async (file, url, onProgress) => {
 
 		if (onProgress) xhr.upload.addEventListener('progress', onProgress);
 
-		xhr.upload.addEventListener('error', reject);
-		xhr.upload.addEventListener('abort', reject);
-		xhr.addEventListener('load', resolve);
+		xhr.upload.addEventListener('error', () => {
+			reject(new Error('Upload errored'));
+		});
+		xhr.upload.addEventListener('abort', () => reject(new Error('Upload aborted.')));
+		xhr.addEventListener('load', () => {
+			if (xhr.status >= 400) {
+				return reject(new Error(`${xhr.status} - Upload failed. ${xhr.responseText}. ${xhr.response}`));
+			}
+			resolve();
+		});
 
 		xhr.open('PUT', url);
 		xhr.send(file);
