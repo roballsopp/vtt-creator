951e069e42879ccce5ea8790605ba5d7516e9b61
rob <rca06d@my.fsu.edu>
2020-10-16 11:44:37 -0600

    let aws handle storage

--- src/config.js
+++ src/config.js
@@ -24,5 +24,8 @@ export const TranscriptionCost = 0.15; // $0.15 per minute
 export const cognitoUserPool = new CognitoUserPool({
 	UserPoolId: CognitoUserPoolId, // Your user pool id here
 	ClientId: CognitoClientId,
-	Storage: localStorage,
+	// we could force local storage here, but aws uses localStorage by default already and
+	//   also does some nice checking and falls back to in memory storage if localStorage isn't available
+	//   https://github.com/aws-amplify/amplify-js/blob/master/packages/amazon-cognito-identity-js/src/StorageHelper.js#L70
+	// Storage: localStorage,
 });
