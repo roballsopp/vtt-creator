030e32eee137439a22d2f5387f894a85734f3d94
Rob Allsopp <rallsopp06@gmail.com>
2021-08-07 01:34:34 -0600

    theres a prop for this now

--- src/common/button.component.js
+++ src/common/button.component.js
@@ -2,29 +2,18 @@ import * as React from 'react'
 import * as PropTypes from 'prop-types'
 import MuiButton from '@material-ui/core/Button'
 import CircularProgress from '@material-ui/core/CircularProgress'
-import {makeStyles} from '@material-ui/styles'
-
-const useStyles = makeStyles({
-	loaderRoot: {
-		margin: -4,
-		marginRight: 4,
-	},
-})
 
 Button.propTypes = {
 	...MuiButton.propTypes,
 	loading: PropTypes.bool,
-	icon: PropTypes.node,
-	onClick: PropTypes.func,
 	name: PropTypes.string,
 }
 
 export default function Button(props) {
-	const {children, icon, loading, name, onClick, ...buttonProps} = props
-	const classes = useStyles()
+	const {children, loading, name, onClick, ...buttonProps} = props
 
 	const handleClick = (...args) => {
-		if (onClick) onClick(...args)
+		onClick?.(...args)
 		window.gtag('event', name, {
 			event_category: 'button_click',
 		})
@@ -32,23 +21,10 @@ export default function Button(props) {
 
 	if (loading) {
 		return (
-			<MuiButton {...buttonProps} disabled>
-				<CircularProgress size={20} variant="indeterminate" color="inherit" className={classes.loaderRoot} />
-				{children}
-			</MuiButton>
-		)
-	}
-
-	if (icon) {
-		return (
-			<MuiButton onClick={handleClick} {...buttonProps}>
-				{React.cloneElement(icon, {
-					fontSize: 'small',
-					style: {
-						margin: -4,
-						marginRight: 4,
-					},
-				})}
+			<MuiButton
+				{...buttonProps}
+				startIcon={<CircularProgress size={20} variant="indeterminate" color="inherit" />}
+				disabled>
 				{children}
 			</MuiButton>
 		)
