6132c8f6206694bf42eca87ff281cfd8131e45f7
Rob Allsopp <rallsopp06@gmail.com>
2021-08-07 15:41:08 -0600

    add note about translation feature on splash page

--- src/splash/splash.component.js
+++ src/splash/splash.component.js
@@ -1,11 +1,13 @@
 import React from 'react'
 import {useHistory} from 'react-router-dom'
-import Typography from '@material-ui/core/Typography'
+import {Box, Container, Grid, Typography} from '@material-ui/core'
 import {makeStyles} from '@material-ui/styles'
 import muiBlueGreys from '@material-ui/core/colors/blueGrey'
+import CloudUploadIcon from '@material-ui/icons/CloudUploadOutlined'
 import CloudDownloadIcon from '@material-ui/icons/CloudDownloadOutlined'
 import MemoryIcon from '@material-ui/icons/MemorySharp'
 import SubtitlesIcon from '@material-ui/icons/Subtitles'
+import TranslateIcon from '@material-ui/icons/Translate'
 import {Button} from '../common'
 import {VC as VCIcon} from '../common/icons'
 import BannerBgImg from '../../assets/banner_bg.jpg'
@@ -15,75 +17,25 @@ const useStyles = makeStyles({
 	root: {
 		display: 'flex',
 		flexDirection: 'column',
-		position: 'absolute',
-		top: 0,
-		right: 0,
-		bottom: 0,
-		left: 0,
-	},
-	main: {
-		display: 'flex',
-		flexDirection: 'column',
-		flex: 1,
-		alignItems: 'center',
-	},
-	toolbar: {
-		display: 'flex',
-		flexDirection: 'row',
-		justifyContent: 'space-between',
-		alignItems: 'center',
-		margin: 'auto',
-		width: 1024,
-		height: 64,
-	},
-	title: {
-		display: 'flex',
-		flexDirection: 'row',
-		alignItems: 'center',
-		marginBottom: 20,
+		height: '100%',
+		minHeight: 0,
+		minWidth: 0,
 	},
 	titleIcon: {
 		marginRight: 16,
 		fontSize: 68,
 	},
-	tag: {
-		marginBottom: 36,
-	},
 	banner: {
-		display: 'flex',
-		flexDirection: 'column',
-		justifyContent: 'center',
-		alignItems: 'center',
 		width: '100%',
 		backgroundPosition: 'center top',
 		backgroundImage: `url(${BannerBgImg})`,
 		backgroundColor: muiBlueGreys[900],
 		color: 'white',
-		padding: 60,
 	},
 	bannerButtonContainer: {
 		display: 'flex',
 		justifyContent: 'center',
 		alignItems: 'center',
-	},
-	bannerButton: {
-		padding: '16px 36px',
-		fontSize: 18,
-	},
-	content: {
-		flex: 1,
-		display: 'flex',
-		justifyContent: 'center',
-	},
-	features: {
-		display: 'flex',
-		justifyContent: 'space-between',
-		width: 1024,
-		margin: '50px 0',
-	},
-	feature: {
-		width: 300,
-		textAlign: 'center',
 	},
 	featureIcon: {
 		fontSize: 80,
@@ -96,49 +48,109 @@ export default function Splash() {
 
 	return (
 		<div className={classes.root}>
-			<main className={classes.main}>
+			<Box component="main" display="flex" flex={1} flexDirection="column" alignItems="center">
 				<div className={classes.banner}>
-					<div className={classes.title}>
-						<VCIcon className={classes.titleIcon} />
-						<Typography variant="h2" color="inherit">
-							VTT Creator
-						</Typography>
-					</div>
-					<Typography variant="h6" className={classes.tag}>
-						An online editor and visualizer for video captions and subtitles.
-					</Typography>
-					<div className={classes.bannerButtonContainer}>
-						<Button
-							name="Create Captions"
-							size="large"
-							variant="contained"
-							color="secondary"
-							onClick={() => history.push('/editor')}>
-							Create Captions
-						</Button>
-					</div>
+					<Box maxWidth={1024} py={8} px={4} margin="auto" overflow="hidden">
+						<Grid container spacing={8}>
+							<Grid item container xs={12} justifyContent="center" alignItems="center">
+								<VCIcon className={classes.titleIcon} />
+								<Typography variant="h2" align="center" color="inherit">
+									VTT Creator
+								</Typography>
+							</Grid>
+							<Grid item container xs={12} justifyContent="center" alignItems="center">
+								<Typography variant="h6" align="center">
+									An online editor and visualizer for video captions and subtitles.
+								</Typography>
+							</Grid>
+							<Grid item container xs={12} justifyContent="center" alignItems="center">
+								<Button
+									name="Create Captions"
+									size="large"
+									variant="contained"
+									color="secondary"
+									onClick={() => history.push('/editor')}>
+									Create Captions
+								</Button>
+							</Grid>
+						</Grid>
+					</Box>
 				</div>
-				<div className={classes.content}>
-					<div className={classes.features}>
-						<div className={classes.feature}>
+				<Box maxWidth={1024} py={8} px={4} overflow="hidden">
+					<Grid container spacing={8}>
+						<Grid
+							item
+							container
+							xs={12}
+							sm={6}
+							lg={4}
+							direction="column"
+							alignItems="center"
+							justifyContent="flex-start">
 							<SubtitlesIcon className={classes.featureIcon} />
-							<Typography variant="h6">Load a video, and see your captions in action as you edit.</Typography>
-						</div>
-						<div className={classes.feature}>
+							<Typography variant="subtitle1" align="center">
+								Load a video, and see your captions in action as you edit.
+							</Typography>
+						</Grid>
+						<Grid
+							item
+							container
+							xs={12}
+							sm={6}
+							lg={4}
+							direction="column"
+							alignItems="center"
+							justifyContent="flex-start">
+							<CloudUploadIcon className={classes.featureIcon} />
+							<Typography variant="subtitle1" align="center">
+								Import and edit existing .vtt and .srt files from your computer.
+							</Typography>
+						</Grid>
+						<Grid
+							item
+							container
+							xs={12}
+							sm={6}
+							lg={4}
+							direction="column"
+							alignItems="center"
+							justifyContent="flex-start">
 							<MemoryIcon className={classes.featureIcon} />
-							<Typography variant="h6">
+							<Typography variant="subtitle1" align="center">
 								Leverage machine learning to automatically extract captions directly from your video.
 							</Typography>
-						</div>
-						<div className={classes.feature}>
+						</Grid>
+						<Grid
+							item
+							container
+							xs={12}
+							sm={6}
+							lg={4}
+							direction="column"
+							alignItems="center"
+							justifyContent="flex-start">
+							<TranslateIcon className={classes.featureIcon} />
+							<Typography variant="subtitle1" align="center">
+								Translate your captions automatically using Google Translate.
+							</Typography>
+						</Grid>
+						<Grid
+							item
+							container
+							xs={12}
+							sm={6}
+							lg={4}
+							direction="column"
+							alignItems="center"
+							justifyContent="flex-start">
 							<CloudDownloadIcon className={classes.featureIcon} />
-							<Typography variant="h6">
+							<Typography variant="subtitle1" align="center">
 								Export your work to a .vtt file for use anywhere on the web, or a .srt file for use offline.
 							</Typography>
-						</div>
-					</div>
-				</div>
-			</main>
+						</Grid>
+					</Grid>
+				</Box>
+			</Box>
 			<Footer />
 		</div>
 	)
