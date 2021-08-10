878551a857f34814eae7a80b1880a10231aa5f22
Rob Allsopp <rallsopp06@gmail.com>
2021-08-09 21:44:16 -0600

    no need for the separate dialog thing

--- src/editor/CueExtractionButton/index.js
+++ src/editor/CueExtractionButton/index.js
@@ -1,12 +1,5 @@
 import ExtractFromVideoButton from './ExtractFromVideoButton'
 import ExtractFromVideoMenuItem from './ExtractFromVideoMenuItem'
 import {ExtractFromVideoProvider, useExtractFromVideo} from './ExtractFromVideoContext'
-import ExtractFromVideoDialogs from './ExtractFromVideoDialogs'
 
-export {
-	ExtractFromVideoButton,
-	ExtractFromVideoMenuItem,
-	ExtractFromVideoDialogs,
-	ExtractFromVideoProvider,
-	useExtractFromVideo,
-}
+export {ExtractFromVideoButton, ExtractFromVideoMenuItem, ExtractFromVideoProvider, useExtractFromVideo}
