3458e711d14e927380a58b565de1e982970a5013
Rob Allsopp <rallsopp06@gmail.com>
2021-08-11 18:15:12 -0600

    refs to reduce renders

--- src/common/usePromiseLazyQuery.js
+++ src/common/usePromiseLazyQuery.js
@@ -5,12 +5,19 @@ export default function usePromiseLazyQuery(query, options) {
 	const apolloClient = useApolloClient()
 	const [queryState, setQueryState] = React.useState({loading: false, error: undefined, data: undefined})
 
+	// prevent changes to the query or options from creating a new instance of run
+	const queryRef = React.useRef()
+	queryRef.current = query
+
+	const optionsRef = React.useRef()
+	optionsRef.current = options
+
 	const run = React.useCallback(() => {
 		setQueryState(s => ({...s, loading: true}))
 		return apolloClient
 			.query({
-				query,
-				...options,
+				query: queryRef.current,
+				...optionsRef.current,
 			})
 			.then(resp => {
 				setQueryState(resp)
@@ -20,7 +27,7 @@ export default function usePromiseLazyQuery(query, options) {
 				setQueryState(s => ({...s, loading: false, error}))
 				throw error
 			})
-	}, [apolloClient, query, options])
+	}, [apolloClient])
 
 	return [run, queryState]
 }
