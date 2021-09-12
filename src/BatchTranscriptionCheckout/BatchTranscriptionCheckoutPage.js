0d00272f09ec64052413831269b5521698de7c92
Rob Allsopp <rallsopp06@gmail.com>
2021-09-12 09:28:54 -0600

    move upload provider higher so uploads continue when you leave the batch page

--- src/BatchTranscriptionCheckout/BatchTranscriptionCheckoutPage.js
+++ src/BatchTranscriptionCheckout/BatchTranscriptionCheckoutPage.js
@@ -1,16 +1,13 @@
 import * as React from 'react'
 import {useParams} from 'react-router-dom'
 import PageContainer from '../common/PageContainer'
-import {UploadProvider} from './UploadProvider'
 import BatchTranscriptionCart from './BatchTranscriptionCartContainer'
 
 export default function BatchTranscriptionCheckoutPage() {
 	const {batchId} = useParams()
 	return (
 		<PageContainer>
-			<UploadProvider>
-				<BatchTranscriptionCart batchId={batchId} />
-			</UploadProvider>
+			<BatchTranscriptionCart batchId={batchId} />
 		</PageContainer>
 	)
 }
